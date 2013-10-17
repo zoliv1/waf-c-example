a510b1b121d947e0cf346c5b93451c24defa0903
Olivier GARRIGUES <garrigues@lagep.univ-lyon1.fr>
2013-10-17 16:11:35 +0200

    ajout des greetings

--- app/src/app.c
+++ app/src/app.c
@@ -19,6 +19,8 @@ int main(int argc, char **argv) {
 
   libb_goodbye();
 
+  liba_greetings();
+
   return 0;
 }
 
--- liba/src/liba.c
+++ liba/src/liba.c
@@ -6,4 +6,7 @@ void liba_hello() {
   fprintf(stdout, "%s\n", "hello from liba");
 }
 
+liba_greetings(){
+   printf("je fais ce qui me plait\n");
+}
 /* EOF */
